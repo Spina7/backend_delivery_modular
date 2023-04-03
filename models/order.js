cbe7092ba95a99ea87aefec24948af38ac584a7e
Edu Torres <eduardocruz280599@gmail.com>
2023-04-02 21:33:38 -0600

    order module part 4.5

--- models/order.js
+++ models/order.js
@@ -7,7 +7,7 @@ Order.findByStatus = (status, result) => {
 
     const sql = `
         SELECT 
-	        CONVERT(O.id, char) AS id, 
+            CONVERT(O.id, char) AS id, 
             CONVERT(O.id_client, char) AS id_client,
             CONVERT(O.id_address, char) AS id_address,
             CONVERT(O.id_delivery, char) AS id_delivery,
@@ -16,53 +16,66 @@ Order.findByStatus = (status, result) => {
             O.lat,
             O.lng,
             JSON_OBJECT(
-		        'id', CONVERT(A.id, char),
+                'id', CONVERT(A.id, char),
                 'address', A.address,
                 'neighborhood', A.neighborhood,
                 'lat', A.lat,
                 'lng', A.lng
             ) AS address,
             JSON_OBJECT(
-		        'id', CONVERT(U.id, char),
+                'id', CONVERT(U.id, char),
                 'name', U.name,
                 'lastname', U.lastname,
                 'image', U.image,
                 'phone', U.phone
             ) AS client,
-	        JSON_ARRAYAGG(
-		        JSON_OBJECT(
-			        'id', CONVERT(P.id, char),
-			        'name', P.name,
-			        'description', P.description,
-			        'image1', P.image1,
+            JSON_OBJECT(
+                'id', CONVERT(U2.id, char),
+                'name', U2.name,
+                'lastname', U2.lastname,
+                'image', U2.image,
+                'phone', U2.phone
+            ) AS delivery,
+            JSON_ARRAYAGG(
+                JSON_OBJECT(
+                    'id', CONVERT(P.id, char),
+                    'name', P.name,
+                    'description', P.description,
+                    'image1', P.image1,
                     'image2', P.image2,
                     'image3', P.image3,
                     'price', P.price,
                     'quantity', OHP.quantity
-		        )
-	        ) AS products
+                )
+            ) AS products
         FROM
-	        orders AS O
+            orders AS O
         INNER JOIN 
-	        users AS U
+            users AS U
         ON 
-	        U.id = O.id_client
+            U.id = O.id_client
+        LEFT JOIN
+            users AS U2
+        ON
+            U2.id = O.id_delivery
         INNER JOIN 
-	        address AS A 
+            address AS A 
         ON 
-	        A.id = O.id_address
+            A.id = O.id_address
         INNER JOIN
-	        order_has_products AS OHP
+            order_has_products AS OHP
         ON 
-	        OHP.id_order = O.id
+            OHP.id_order = O.id
         INNER JOIN
-	        products AS P
+            products AS P
         ON 
-	        P.id = OHP.id_product
+            P.id = OHP.id_product
         WHERE 
-	        status = ?
+            status = ?
         GROUP BY  
-	        O.id;
+            O.id
+        ORDER BY
+            O.timestamp;
     `;
 
     db.query(
